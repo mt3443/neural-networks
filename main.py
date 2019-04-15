056c762f30e13fd7e7d5e765239bc4d75262e3fd
Matthew Taylor <mjt@ku.edu>
2019-04-15 16:54:04 -0500

    added batching

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from nn import NeuralNetwork
 
 mndata = MNIST('data/')
 mndata.gz = True
-digits_train, labels_train = mndata.load_training()
-digits_test, labels_test = mndata.load_testing()
+train_input, train_output = mndata.load_training()
+test_input, test_output = mndata.load_testing()
 
-n_input_nodes = 784
-n_output_nodes = 10
-hidden_layers = [100]
+# n_input_nodes = 784
+# n_output_nodes = 10
+# hidden_layers = [100]
+
+n_input_nodes = 3
+n_output_nodes = 2
+hidden_layers = [4]
+
+train_input = [[1, 0, 2], [2, 4, 6], [2, 3, 1], [1, 2, 4], [2, 1, 4], [0, 0, 2]]
+train_output = [0, 1, 0, 1, 1, 1]
 
 n = NeuralNetwork(n_input_nodes, n_output_nodes, hidden_layers)
 
-n_epochs = 20
-n.train(digits_train, labels_train, n_epochs)
-
-# print error and implement learning rate
+n_epochs = 1
+n.train(train_input, train_output, n_epochs, batch_size=2)
--- nn.py
+++ nn.py
@@ -1,5 +1,12 @@
+import random
 import numpy as np
 import sys
+
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+	return x * (1 - x)
 
 class NeuralNetwork:
 
@@ -8,7 +15,7 @@ class NeuralNetwork:
 		self.n_input_nodes = n_input_nodes
 		self.n_output_nodes = n_output_nodes
 		self.hidden_layers = hidden_layers
-		self.layers = [None] * (len(hidden_layers) + 1)
+		self.activations = [None] * (len(hidden_layers) + 1)
 		self.weights = weights
 
 		# assign initial weights if none are given (i.e. if network is not trained)
@@ -26,11 +33,11 @@ class NeuralNetwork:
 
 		for i in range(len(self.hidden_layers) + 1):
 			if i == 0:
-				weights.append(np.random.rand(self.n_input_nodes, self.hidden_layers[0]))
+				weights.append(np.random.randn(self.n_input_nodes, self.hidden_layers[0]))
 			elif i != len(self.hidden_layers):
-				weights.append(np.random.rand(self.hidden_layers[i - 1], self.hidden_layers[i]))
+				weights.append(np.random.randn(self.hidden_layers[i - 1], self.hidden_layers[i]))
 			else:
-				weights.append(np.random.rand(self.hidden_layers[-1], self.n_output_nodes))
+				weights.append(np.random.randn(self.hidden_layers[-1], self.n_output_nodes))
 
 		# reset rng (deseed)
 		np.random.seed()
@@ -47,34 +54,67 @@ class NeuralNetwork:
 			if expected_weights[i].shape != self.weights[i].shape:
 				sys.exit('Error: given weights do not match neural network dimensions')
 
-	def sigmoid(self, x):
-		return 1 / (1 + np.exp(-x))
-
-	def sigmoid_derivative(self, x):
-		return x * (1 - x)
-
-	def forward(self, x):
-		self.layers[0] = self.sigmoid(np.dot(x, self.weights[0]))
-
+	def feedforward(self, x):
+		self.activations[0] = sigmoid(np.dot(x, self.weights[0]))
+		
 		for i in range(len(self.hidden_layers) - 1):
-			self.layers[i + 1] = self.sigmoid(np.dot(self.layers[i], self.weights[i + 1]))
+			self.activations[i + 1] = sigmoid(np.dot(self.activations[i], self.weights[i + 1]))
 
-		self.layers[-1] = self.sigmoid(np.dot(self.layers[-2], self.weights[-1]))
+		self.activations[-1] = sigmoid(np.dot(self.activations[-2], self.weights[-1]))
+
+	def calculate_error(self, y):
+		error = 0
+
+		for i in range(self.n_output_nodes):
+			error += 0.5 * ((self.activations[-1][i] - y[i]) ** 2)
+
+		return error
 
 	def backpropagation(self, x, y):
-		# implement me
-		a = 3
+		error = (y - self.activations[-1]) * sigmoid_derivative(self.activations[-1])
 
-	def train(self, x, y, epochs):
+		a = np.dot(error, self.weights[1].T) * sigmoid_derivative(self.activations[0])
+		dw1 = np.dot(x, a)
+		dw2 = np.dot(self.activations[0], error)
+
+		self.weights[0] += dw1
+		self.weights[1] += dw2
+
+	def train(self, x, y, epochs, batch_size=500):
+
 		for i in range(epochs):
 			print('Epoch', i + 1)
-			for j in range(len(x)):
-				temp_input = x[j]
-				temp_output = np.zeros(10)
-				temp_output[y[j]] = 1
-				self.forward(temp_input)
-				self.backpropagation(temp_input, temp_output)
+
+			# get a batch
+			inputs, outputs = self.getBatch(x, y, batch_size)
+
+			# normalize inputs
+			temp_inputs = np.array(inputs) / 255
+
+			# convert outputs to one hot
+			temp_outputs = []
+			for j in range(batch_size):
+				a = np.zeros(self.n_output_nodes, dtype=np.int8)
+				a[outputs[j]] = 1
+				temp_outputs.append(a)
+
+			# train network
+			for j in range(batch_size):
+				self.feedforward(temp_inputs[j])
+				self.backpropagation(temp_inputs[j], temp_outputs[j])
+
+	def getBatch(self, x,  y, batch_size):
+		inputs = []
+		outputs = []
+
+		sample_indices = random.sample(range(len(x)), batch_size)
+
+		for index in sample_indices:
+			inputs.append(x[index])
+			outputs.append(y[index])
+
+		return inputs, outputs
 
 	def predict(self, x):
-		self.forward(x)
-		return np.argmax(self.layers[-1])
+		self.feedforward(x)
+		return np.argmax(self.activations[-1])
